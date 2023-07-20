92317967dd895e0f91e85444cb63742675dc0a31
jarnalyrkar <joachimsolstrom@gmail.com>
2023-07-20 09:14:44 +0200

    Add new readme image

--- index.php
+++ index.php
@@ -35,9 +35,9 @@
   </main>
   <footer>
     <div class="footer-buttons">
-      <button class="action-button" data-action="settings">&#9881;</button>
-      <button class="action-button" data-action="stop">&block;</button>
-      <button class="action-button" data-action="info">&#8505;</button>
+      <button class="action-button" title="Settings" data-action="settings">&#9881;</button>
+      <button class="action-button" title="Stop all tracks" data-action="stop">&block;</button>
+      <button class="action-button" title="Info about the program" data-action="info">&#8505;</button>
     </div>
     <div class="footer-sliders">
       <div class="footer-slider">
